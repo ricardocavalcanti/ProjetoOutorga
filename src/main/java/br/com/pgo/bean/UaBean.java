a5557a0c757b31c2b12893fd7b4eec495e498918
Ricardo <Ricardo@192.168.25.6>
2016-06-16 19:57:14 -0300

    Intalação OmniFaces e uso do Marge

--- src/main/java/br/com/pgo/bean/UaBean.java
+++ src/main/java/br/com/pgo/bean/UaBean.java
@@ -1,18 +1,15 @@
 package br.com.pgo.bean;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
+
+import org.omnifaces.util.Messages;
 
 @ManagedBean
 public class UaBean {
 
 	public void salvar() {
 
-		String texto = "Programação WEB com Java";
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, texto, texto);
-		FacesContext contexto = FacesContext.getCurrentInstance();
-		contexto.addMessage(null, message);
+		Messages.addGlobalInfo("Programação Java!");
 
 	}
 
