65a6642bf11b4aeef58f1952054db50583433f02
Vaderli <Vaderli@192.168.100.40>
2016-06-17 13:50:25 -0300

    Ua teste tela cadastro.

--- src/main/java/br/com/pgo/bean/UaBean.java
+++ src/main/java/br/com/pgo/bean/UaBean.java
@@ -1,16 +1,36 @@
 package br.com.pgo.bean;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
 
+import br.com.pgo.domain.Ua;
+
 @ManagedBean
+@ViewScoped //Tempo de vida do objeto
 public class UaBean {
+
+	private Ua ua;
+
+	public Ua getUa() {
+		return ua;
+	}
+
+	public void setUa(Ua ua) {
+		this.ua = ua;
+	}
+	
+	public void novo(){
+		
+		ua = new Ua();
+	}
 
 	public void salvar() {
 
-		Messages.addGlobalInfo("Programação Java!");
-
+		Messages.addGlobalInfo(
+				"Numero UA: " + ua.getNumeroUa() + "-" + "Ano UA: " + ua.getAno() + "-" 
+		+ "JAN: " + ua.getJan());
 	}
 
 }
