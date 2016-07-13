fb9edee86b58c7cd8923a7f51f4649ac49264ffa
Vaderli <Vaderli@10.10.0.87>
2016-07-13 16:55:14 -0300

    Remodelagem calculo vazao.

--- src/main/java/br/com/pgo/bean/ItemDeVendaBean.java
+++ src/main/java/br/com/pgo/bean/ItemDeVendaBean.java
@@ -20,24 +20,14 @@ public class ItemDeVendaBean implements Serializable {
 
 	private Ua ua;
 	private List<Ua> listaUa;
-	private List<ItemDeVenda> itensVenda;
+	private List<ItemDeVenda> listaItensVenda;
 	private ItemDeVenda demandaPontual;
 	private ItemDeVenda itemDeVenda;
-	private double valor;
-	private double resultado;
 
 	public void novo() {
 
 		itemDeVenda = new ItemDeVenda();
 
-	}
-
-	public ItemDeVenda getItemDeVenda() {
-		return itemDeVenda;
-	}
-
-	public void setItemDeVenda(ItemDeVenda itemDeVenda) {
-		this.itemDeVenda = itemDeVenda;
 	}
 
 	public Ua getUa() {
@@ -56,12 +46,12 @@ public class ItemDeVendaBean implements Serializable {
 		this.listaUa = listaUa;
 	}
 
-	public List<ItemDeVenda> getItensVenda() {
-		return itensVenda;
+	public List<ItemDeVenda> getListaItensVenda() {
+		return listaItensVenda;
 	}
 
-	public void setItensVenda(List<ItemDeVenda> itensVenda) {
-		this.itensVenda = itensVenda;
+	public void setListaItensVenda(List<ItemDeVenda> listaItensVenda) {
+		this.listaItensVenda = listaItensVenda;
 	}
 
 	public ItemDeVenda getDemandaPontual() {
@@ -72,20 +62,12 @@ public class ItemDeVendaBean implements Serializable {
 		this.demandaPontual = demandaPontual;
 	}
 
-	public double getValor() {
-		return valor;
+	public ItemDeVenda getItemDeVenda() {
+		return itemDeVenda;
 	}
 
-	public void setValor(double valor) {
-		this.valor = valor;
-	}
-
-	public double getResultado() {
-		return resultado;
-	}
-
-	public void setResultado(double resultado) {
-		this.resultado = resultado;
+	public void setItemDeVenda(ItemDeVenda itemDeVenda) {
+		this.itemDeVenda = itemDeVenda;
 	}
 
 	@PostConstruct
@@ -95,44 +77,13 @@ public class ItemDeVendaBean implements Serializable {
 			UaDAO uaDAO = new UaDAO();
 			listaUa = uaDAO.listar("numeroUa");
 
-			itensVenda = new ArrayList<>();
+			listaItensVenda = new ArrayList<>();
 
 		} catch (RuntimeException erro) {
 
 			Messages.addGlobalInfo("Erro ao tentar carregar 'Seleção UA'");
 			erro.printStackTrace();
 		}
-
-	}
-
-	public void calcular() {
-
-		double[] meses = new double[12];
-
-		meses[0] = ua.getJan();
-		meses[1] = ua.getFev();
-		meses[2] = ua.getMar();
-		meses[3] = ua.getAbr();
-		meses[4] = ua.getMai();
-		meses[5] = ua.getJun();
-		meses[6] = ua.getJul();
-		meses[7] = ua.getAgo();
-		meses[8] = ua.getSet();
-		meses[9] = ua.getOut();
-		meses[10] = ua.getNov();
-		meses[11] = ua.getDez();
-
-		double cont = 0;
-		for (int i = 0; i <= 11; i++) {
-
-			if (valor <= meses[i]) {
-
-				cont++;
-			}
-
-		}
-
-		resultado = (cont / 12);
 
 	}
 
@@ -152,23 +103,4 @@ public class ItemDeVendaBean implements Serializable {
 
 	}
 
-	public void adicionar() {
-
-		novo();
-
-		itemDeVenda.setNumeroUa(ua);
-		itemDeVenda.setDemandaPontual(valor);
-		itemDeVenda.setGarantiaVazao(resultado);
-
-		itensVenda.add(itemDeVenda);
-
-		for (ItemDeVenda venda : itensVenda) {
-
-			System.out.println("Demanda Pontual: " + venda.getDemandaPontual());
-			System.out.println("Garantia Vazao: " + venda.getGarantiaVazao());
-			System.out.println("Numero Ua: " + venda.getNumeroUa());
-
-		}
-
-	}
 }
