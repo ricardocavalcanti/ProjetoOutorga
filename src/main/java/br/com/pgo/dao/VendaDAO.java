32855f2a123edced4a08e8e535c84c5dfcf882ae
Ricardo <ricardocavalcanti@outlook.com>
2016-08-27 23:01:40 -0300

    HQL para buscar por numero de UA

--- src/main/java/br/com/pgo/dao/VendaDAO.java
+++ src/main/java/br/com/pgo/dao/VendaDAO.java
@@ -1,7 +1,32 @@
 package br.com.pgo.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import br.com.pgo.domain.Ua;
 import br.com.pgo.domain.Venda;
+import br.com.pgo.util.HibernateUtil;
 
 public class VendaDAO extends GenericDAO<Venda> {
+
+	public List<Ua> buscarUa(int numeroUa) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+
+		try {
+			
+			Query consulta = sessao.createQuery("from Ua where numeroUa=?");
+			consulta.setParameter(0, numeroUa);
+			List<Ua> resultado = consulta.list();
+			return resultado;
+
+		} catch (RuntimeException erro) {
+
+			throw erro;
+
+		} finally {
+			sessao.close();
+		}
+	}
 
 }
---
+++ src/test/java/br/com/pgo/dao/VendaDAOTest.java
@@ -0,0 +1,42 @@
+package br.com.pgo.dao;
+
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.com.pgo.domain.Ua;
+
+public class VendaDAOTest {
+
+	
+	
+	@Test	
+	public void buscar() {
+
+		int numeroUa = 0;
+		
+
+		VendaDAO BuscarUA = new VendaDAO();
+		List<Ua> lista = BuscarUA.buscarUa(numeroUa);
+
+		if (lista.isEmpty()) {
+			
+			System.out.println("Nenhum registro encontrado");
+			
+		} else {
+            int i=0;
+			for (Ua ua : lista) {
+				i++;
+				
+				
+				System.out.println(ua.getNumeroUa());
+				System.out.println("Ano da UA: "+ua.getAno());
+				
+			}
+			System.out.println("Quantidade Ua encontrada: "+i);
+		}
+
+	}
+	
+}
