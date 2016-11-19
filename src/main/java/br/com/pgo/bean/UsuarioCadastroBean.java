ebb9571ea836cfba342d7dab541feecf39f3f255
Ricardo <Ricardo@PCNINJA>
2016-11-18 21:25:24 -0300

    Correcao bug

--- src/main/java/br/com/pgo/bean/UsuarioCadastroBean.java
+++ src/main/java/br/com/pgo/bean/UsuarioCadastroBean.java
@@ -1,15 +1,17 @@
 package br.com.pgo.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.faces.bean.ManagedBean;
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 
 import org.omnifaces.util.Messages;
 
+import br.com.pgo.dao.PessoaDAO;
 import br.com.pgo.dao.UsuarioDAO;
-
+import br.com.pgo.domain.Pessoa;
 import br.com.pgo.domain.Usuario;
 
 @SuppressWarnings("serial")
@@ -19,6 +21,8 @@ public class UsuarioCadastroBean implements Serializable {
 
 	private Usuario usuario;
 	private UsuarioDAO usuarioDAO;
+	private PessoaDAO pessoaDAO;
+	private List<Pessoa> listaPessoa;
 
 	public Usuario getUsuario() {
 		return usuario;
@@ -26,15 +30,39 @@ public class UsuarioCadastroBean implements Serializable {
 
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
+	}	
+	
+
+	public List<Pessoa> getListaPessoa() {
+		return listaPessoa;
+	}
+
+	public void setListaPessoa(List<Pessoa> listaPessoa) {
+		this.listaPessoa = listaPessoa;
 	}
 
 	@PostConstruct
 	public void iniciar() {
 
 		usuarioDAO = new UsuarioDAO();
+		pessoaDAO = new PessoaDAO();
 		usuario = new Usuario();
 	}
 
+	public void carregarLista(){
+		
+		try{
+			
+			listaPessoa = pessoaDAO.listar();
+			
+		}catch (RuntimeException erro){
+			
+			Messages.addFlashGlobalInfo("Erro ao carregar lista de pessoas");
+			erro.printStackTrace();
+		}
+		
+	}
+	
 	public void salvar() {
 
 		try {
